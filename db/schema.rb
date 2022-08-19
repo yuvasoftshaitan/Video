8bfae9651442765fcb423a753c3c84c3627c2df8
shaitan <Shaitanyuvasoft358@gmaiil.com>
2022-08-19 12:28:51 +0530

    add db settings

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_08_18_010105) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.string "last_name"
